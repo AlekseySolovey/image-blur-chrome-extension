1f6f4948d7dd8c3b2a910f52648b0a340443ad43
Aleksey Solovey <alekseysolovey@hotmail.co.uk>
2021-07-08 18:14:27 +0100

    Blur/unblur images when the icon is toggled

--- background.js
+++ background.js
@@ -1,9 +1,25 @@
 // Listen for browser / page actions and their events.
+// Runs as a service worker when Chrome is launched
 
-console.log("Service worker running.");
+console.log("Image Blur extension is running.");
 
-var isChrome = typeof chrome !== "undefined" && typeof browser === "undefined";
-var ext = isChrome ? chrome : browser; // Extension API
+let isChrome = typeof chrome !== "undefined" && typeof browser === "undefined";
+let ext = isChrome ? chrome : browser; // Extension API
 
-// TODO: add an event listener to a button and 
+// TODO: add an event listener to a button and
 //       execute code in content.js
+
+let action = isChrome ? chrome.action : browser.browserAction;
+
+action.onClicked.addListener(buttonClicked);
+
+let blur = true;
+
+function buttonClicked(tab) {
+  let msg = {
+    text: "blur-toggle",
+    blur: blur,
+  };
+  blur = !blur; // toggle between true and false
+  chrome.tabs.sendMessage(tab.id, msg);
+}
--- content.js
+++ content.js
@@ -1,6 +1,33 @@
+class Logger {
+  dev = true; // true -> development; false -> production
+
+  log(msg, type = "log") {
+    if (this.dev == false) return;
+    switch (type) {
+      case "log":
+        console.log(msg);
+        break;
+      case "warn":
+        console.warn(msg);
+        break;
+      case "error":
+        console.error(msg);
+        break;
+      case "table":
+        console.table(msg);
+        break;
+      default:
+        console.log(msg);
+    }
+  }
+}
+
 // Execute and manipulate code when the page loads.
 
-console.log("Chrome extension is active.");
+const logger = new Logger();
+logger.dev = false;
+
+logger.log("Chrome extension is active.");
 
 // TODO: add a listener for a button from the background service worker
 //       to blur out images only when you need to and not immediately
@@ -18,24 +45,57 @@ let imageTransition = 0.3;
 
 // Custom class with a blur effect and a transition
 const cssClass = `._image_blur_class{ \
-filter:blur(${imageBlur}px) opacity(${imageOpacity}%) \
-contrast(${imageContrast}%); \
-transition:${imageTransition}s; transition-timing-function:ease-in; }`;
+  filter:blur(${imageBlur}px) opacity(${imageOpacity}%) \
+  contrast(${imageContrast}%); \
+  transition:${imageTransition}s; transition-timing-function:ease-in; }`;
 // revert back to original image
 const cssOnHover = "._image_blur_class:hover{filter:none;}";
 const mergedCss = cssClass + " " + cssOnHover;
 
-// create and push a new style tag with custom css
-const style = document.createElement("style");
-if (style.styleSheet) {
-  style.styleSheet.cssText = mergedCss;
-} else {
-  style.appendChild(document.createTextNode(mergedCss));
-}
-document.getElementsByTagName("head")[0].appendChild(style);
+function addBlurEffect() {
+  // create and push a new style tag
+  const style = document.createElement("style");
 
-// add a new css class to all images
-let images = document.getElementsByTagName("img");
-for (img of images) {
-  img.className += " _image_blur_class";
+  // add the custom css to the new style (only when needed)
+  if (style.styleSheet) {
+    style.styleSheet.cssText = mergedCss;
+  } else {
+    style.appendChild(document.createTextNode(mergedCss));
+  }
+  document.getElementsByTagName("head")[0].appendChild(style);
+
+  // add a new css class to all images
+  let images = document.getElementsByTagName("img");
+  for (img of images) {
+    img.className += " _image_blur_class";
+  }
+}
+
+function removeBlurEffect() {
+  // remove "_image_blur_class" from all images
+  let images = document.getElementsByTagName("img");
+  for (img of images) {
+    if (img.classList.contains("_image_blur_class")) {
+      img.classList.remove("_image_blur_class");
+    }
+  }
+}
+
+let isChrome = typeof chrome !== "undefined" && typeof browser === "undefined";
+let ext = isChrome ? chrome : browser; // Extension API
+
+chrome.runtime.onMessage.addListener(messageHandler);
+
+// Message listener for background script
+function messageHandler(request, sender, sendResponse) {
+  if (request.text == "blur-toggle") {
+    // run blur code
+    if (request.blur) {
+      logger.log("Add blur effect");
+      addBlurEffect();
+    } else {
+      logger.log("Remove blur effect");
+      removeBlurEffect();
+    }
+  }
 }
